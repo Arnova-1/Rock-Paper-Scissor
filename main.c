c5df4816e513bbc14042ec5ef897ef05cd5577e8
Arnova-1 <arnova124@gmail.com>
2025-09-08 10:37:36 +0700

    fix rock-paper-scissor logic and improve output message

--- main.c
+++ main.c
@@ -2,23 +2,32 @@
 #include <stdlib.h>
 #include <time.h>
 
+const char* choiceMsg(int choice) {
+    switch (choice) {
+        case 1: return "ROCK";
+        case 2: return "PAPER";
+        case 3: return "SCISSOR";
+        default: return "INVALID";
+    }
+} 
+
 int game(int computer, int user) {
     if (computer == user) {
         return -1;
     }
-    // 0 ROCK 1 PAPER 2 SCISSOR
-    if(computer == 0 && user == 2) {
-        return 0;  // ROCK PAPER (LOSE)
-    } else if (computer == 0 && user == 3) {
-        return 0; // ROCK SCISSOR (LOSE)
-    } else if (computer == 1 && user == 1) {
-        return 1; // PAPER ROCK (WIN)
+    // 1 ROCK 2 PAPER 3 SCISSOR
+    if(computer == 1 && user == 2) {
+        return 1;  // ROCK PAPER (WIN)
     } else if (computer == 1 && user == 3) {
-        return 1; // PAPER SCISSOR (WIN)
+        return 0; // ROCK SCISSOR (LOSE)
     } else if (computer == 2 && user == 1) {
+        return 0; // PAPER ROCK (LOSE)
+    } else if (computer == 2 && user == 3) {
+        return 1; // PAPER SCISSOR (WIN)
+    } else if (computer == 3 && user == 1) {
         return 1; // SCISSOR ROCK (WIN)
-    } else if (computer == 2 && user == 2) {
-        return 0; // SCISSOR PAPER
+    } else if (computer == 3 && user == 2) {
+        return 0; // SCISSOR PAPER (LOSE)
     }
 }
 
@@ -26,9 +35,9 @@ int main() {
     int choice;
     char buffer[16];
 
-    printf("--------------------\n"
+    printf("====================\n"
         " ROCK PAPER SCISSOR\n"
-        "--------------------\n"
+        "====================\n"
         "[1] ROCK\n"
         "[2] PAPER\n"
         "[3] SCISSOR\n"
@@ -51,12 +60,13 @@ int main() {
 
     srand(time(NULL));
 
-    int computerChoice = rand() % 3;
+    int computerChoice = rand() % 3 + 1;
 
     int result = game(computerChoice, choice);
 
-    printf("YOU: %d vs COMPUTER: %d\n", choice, computerChoice);
-    
+    printf("----------------------------------------\n"
+        "YOU: %s vs COMPUTER: %s\n", choiceMsg(choice), choiceMsg(computerChoice));
+
     if(result == -1) {
         printf("Game Draw!\n");
     } else if(result == 1) {
@@ -65,7 +75,7 @@ int main() {
         printf("OH! You have lost the game!\n");
     }
 
-    
+    printf("----------------------------------------\n");
 
     return 0;
 }
